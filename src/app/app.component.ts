c89c271e7084c93ed569d8ad88a8cf2d3c85dbb2
Zesion21 <1462724221@qq.com>
2019-07-31 11:06:12 +0800

    noting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
 import { EventBusService } from './service/event-bus.service';
+import { Component, ViewChild } from '@angular/core';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
